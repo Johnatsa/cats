eb75263bd305b3d88a22315ffcad7b950ef5547a
John Atsalakis”git config --global user.name John <y.atsalakis@gmail.com>
2026-06-27 00:16:57 +0300

    done?

---
+++ src/javato/activetesting/BugLogger.java
@@ -0,0 +1,23 @@
+package javato.activetesting;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+
+public class BugLogger{
+
+    public static synchronized void log(String bugType, Integer threadNum, String iids){
+        String filepath = "/tmp/bugs.txt";
+        String entry = bugType + " " + threadNum + " " + iids;
+        
+        try (PrintWriter out = new PrintWriter(new FileWriter(filepath, true))) {
+            out.println(entry);
+            System.out.println("[LOGGED] " + entry);
+        } catch (IOException e) {
+            System.out.println("[ERROR] Could not write to " + filepath);
+        }
+    }
+
+}
